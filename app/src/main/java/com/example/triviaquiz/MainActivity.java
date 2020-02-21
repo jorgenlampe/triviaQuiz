a9b4e7bcd0a7abce1e780771ff5f8115a43055de
Jørgen Lampe <jla099@post.uit.no>
2020-02-21 15:04:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/triviaquiz/MainActivity.java
@@ -0,0 +1,14 @@
+    package com.example.triviaquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
