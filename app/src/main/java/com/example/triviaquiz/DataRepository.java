3917edc0a53e14e060c29f601d8940ea18fed42e
Martin haram nygård <mny066@uit.no>
2020-02-23 10:11:03 +0100

    implementerte basic dataRepository og DataViewModel

--- app/src/main/java/com/example/triviaquiz/DataRepository.java
+++ app/src/main/java/com/example/triviaquiz/DataRepository.java
@@ -2,23 +2,68 @@ package com.example.triviaquiz;
 
 import android.content.Context;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DataRepository {
-
-
-    public void downloadQuestions(String url, Context context){
-
-
-        Gson gson = new Gson();
-        Type type = new TypeToken<ArrayList<Question>>(){}.getType();
+    private RequestQueue queue = null;
+    private MutableLiveData<List<Question>> questions = new MutableLiveData<>();
+    private MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    public void downloadQuestions(Context context,String url) {
+        //Type type = new TypeToken<ArrayList<Question>>(){}.getType();
      //   ArrayList<Question> tmpList = gson.fromJson(jsonArrayAsString, type);
+        String mUrlString = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";
+        queue = MySingletonQueue.getInstance(context).getRequestQueue();
+        // Laster ned en LISTE med JSON-objekter:
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                mUrlString,
+                null,
+                new Response.Listener<JSONArray>() {
 
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try{Gson gson = new Gson();
+                            // Loop gjennom returnert JSON-array:
+                            ArrayList<Question> tmpList = new ArrayList<>();
+                            for(int i=0; i < response.length(); i++)    {
+                                JSONObject questionsAsJson = response.getJSONObject(i);
+                                Question question = gson.fromJson(questionsAsJson.toString(), Question.class);
+                                tmpList.add(question);
+
+                            }
+                            questions.postValue(tmpList);
+                        }  catch (JSONException e)  {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        errorMessage.postValue(error.getMessage());
+                    }
+                });
+        queue.add(jsonArrayRequest);
     }
+
+    public MutableLiveData<List<Question>> getQuestions() { return questions; }
+    public MutableLiveData<String> getErrorMessage() { return errorMessage; }
 
 
 }
--- app/src/main/java/com/example/triviaquiz/DataViewModel.java
+++ app/src/main/java/com/example/triviaquiz/DataViewModel.java
@@ -1,4 +1,32 @@
 package com.example.triviaquiz;
 
+import android.content.Context;
+
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
 public class DataViewModel {
+
+    private DataRepository mRepository;
+    private MutableLiveData<List<Question>> mQuestions;
+    private MutableLiveData<String> mErrorMessage;
+
+    public DataViewModel() {
+        mRepository = new DataRepository();
+        mQuestions = mRepository.getQuestions();
+        mErrorMessage = mRepository.getErrorMessage();
+    }
+
+    public MutableLiveData<List<Question>> getmQuestions() {
+        return mQuestions;
+    }
+
+    public MutableLiveData<String> getmErrorMessage() {
+        return mErrorMessage;
+    }
+
+    public void downloadQuestions(Context context, String url) {
+        mRepository.downloadQuestions(context, url);
+    }
 }
