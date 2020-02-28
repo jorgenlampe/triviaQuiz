0c06299b290bdd65347ddc2906fdbae694d2c94d
Jørgen Lampe <jla099@post.uit.no>
2020-02-28 21:56:52 +0100

    Laget metode for å hente url fra innstillingene

--- app/src/main/java/com/example/triviaquiz/MainActivity.java
+++ app/src/main/java/com/example/triviaquiz/MainActivity.java
@@ -1,12 +1,14 @@
     package com.example.triviaquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceManager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -23,14 +25,80 @@ import android.widget.Toast;
 
     }
 
+    public String getUrl(SharedPreferences preferences){
+
+        StringBuilder url = new StringBuilder();
+
+        url.append("https://opentdb.com/api.php?amount=");
+
+        url.append(getNumberOfQuestions(preferences));
+        url.append(getCategory(preferences));
+        url.append(getDifficulty(preferences));
+        url.append(getType(preferences));
+
+        return url.toString();
+
+    }
+
+    public String getNumberOfQuestions(SharedPreferences preferences){
+
+        String amount = preferences.getString("amount", "10");
+        return amount;
+    }
+
+    public String getCategory(SharedPreferences preferences){
+
+        String category = preferences.getString("category", "");
+
+        return "&category=" + category;
+    }
+
+    public String getType(SharedPreferences preferences){
+
+        Boolean anyType = preferences.getBoolean("anyType", false);
+        Boolean multipleChoice = preferences.getBoolean("multipleChoice", false);
+        Boolean trueOrFalse = preferences.getBoolean("trueOrFalse", false);
+
+        if (anyType)
+            return "";
+        if (multipleChoice)
+            return "&type=multiple";
+        if (trueOrFalse)
+            return "&type=boolean";
+
+        return "";
+
+    }
+
+    //https://opentdb.com/api.php?amount=10&category=24&difficulty=easy&type=multiple
+
+    public String getDifficulty(SharedPreferences preferences){
+
+        Boolean diffAny = preferences.getBoolean("diffAny", false);
+        Boolean diffEasy = preferences.getBoolean("diffEasy", false);
+        Boolean diffMedium = preferences.getBoolean("diffMedium", false);
+        Boolean diffHard = preferences.getBoolean("diffHard", false);
+
+       if (diffAny)
+           return "";
+       if (diffEasy)
+           return "&difficulty=easy";
+       if (diffMedium)
+           return "&difficulty=medium";
+       if (diffHard)
+           return "&difficulty=hard";
+
+       return "";
+
+    }
+
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
             // TODO Check the shared preference and key parameters
             SharedPreferences myprefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-            //boolean playSounds = myprefs.getBoolean("PREF_PLAY_SOUNDS", false);
+
             String amount = myprefs.getString("amount", "10");
-            //Log.d("DDDD", "AAAA");
-            //. . .
+
             Toast.makeText(this, key + " endret seg...", Toast.LENGTH_SHORT).show();
         }
 
@@ -47,16 +115,6 @@ import android.widget.Toast;
                     Intent intent = new Intent(this, MyPreferenceActivity.class);
                     startActivity(intent);
                     return true;
-
-       /*     case R.id.action_end:
-                finish();
-                break;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-        }*/
 
             }
 
